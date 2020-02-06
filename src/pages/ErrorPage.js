a1f8d5c3a7b7e3708613e212303a0a741a09db18
Octavian David <45077244+octaviandd@users.noreply.github.com>
2020-02-06 22:31:08 +0000

    Update ErrorPage.js

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,21 @@
 /** @format */
-
 import React from "react";
+import Banner from "../components/banner/Banner";
+import Fade from "react-reveal";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
-  return <div>Error component</div>;
+  return (
+    <div className="error">
+      <Fade right>
+        <div>
+          <Banner title="404" subtitle="page not found">
+            <Link to="/" className="btn-primary">
+              Home
+            </Link>
+          </Banner>
+        </div>
+      </Fade>
+    </div>
+  );
 }
